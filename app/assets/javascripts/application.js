1fbc10eea66c67600dce2c0a42c2c54437878d0f
Uwe Kubosch <uwe@kubosch.no>
2014-09-04 12:16:57 +0200

    * La til fremdriftsindikator.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,9 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require nprogress
+//= require nprogress-turbolinks
+NProgress.configure
+  //showSpinner: false
+  ease: 'ease'
+  speed: 500
