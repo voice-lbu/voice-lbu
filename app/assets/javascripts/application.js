0bd24a9639560ee143f8a7320120ade3698aac41
Uwe Kubosch <uwe@kubosch.no>
2014-09-22 19:07:11 +0200

    * Bedre inn og utmelding

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,9 +2,18 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require moment
+//= require moment/nb
+//= require bootstrap-datetimepicker
 //= require nprogress
 //= require nprogress-turbolinks
+//= require_self
 NProgress.configure
   //showSpinner: false
   ease: 'ease'
   speed: 500
+
+$().ready(function () {
+    $('.date').datetimepicker({format: 'YYYY-MM-DD', pickTime: false});
+    $('.datetime').datetimepicker({format: 'YYYY-MM-DD H.mm'});
+});
