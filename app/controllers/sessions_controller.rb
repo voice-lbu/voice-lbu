331da44d167fb8603803a480c9cd8d8c30f778e5
Uwe Kubosch <uwe@kubosch.no>
2014-10-06 20:28:25 +0200

    * Norske tekster for norske brukere.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -38,15 +38,15 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to root_url, :notice => 'Logged in!'
+      redirect_to root_url, :notice => 'Logget inn!'
     else
-      flash.now.alert = 'Invalid email or password'
+      flash.now.alert = 'Feil e-post eller passord.'
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => 'Logged out!'
+    redirect_to root_url, :notice => 'Logget ut!'
   end
 end
